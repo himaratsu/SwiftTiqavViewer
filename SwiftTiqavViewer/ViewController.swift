07ad43241b8a3ca2891702d98bd18bccfb66c6d0
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2014-06-04 19:11:57 +0900

    Initial Commit

---
+++ SwiftTiqavViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTiqavViewer
+//
+//  Created by 平松　亮介 on 2014/06/04.
+//  Copyright (c) 2014年 himara2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
